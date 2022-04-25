0e2553a5e8bb65f651d84fbca77146a34894f401
USER <USER@DESKTOP-290I1GM>
2022-04-25 23:35:04 +0530

    update some meven

--- ElectricityManagmentSystem/src/com/ConcptionPaths.java
+++ ElectricityManagmentSystem/src/com/ConcptionPaths.java
@@ -1,5 +1,28 @@
 package com;
 
+import model.Item;
+//For REST Service
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+//For JSON
+import com.google.gson.*; 
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document;
+//start
+//import javax.websocket.server.PathParam;
+import javax.ws.rs.GET; 
+import javax.ws.rs.Path; 
+import javax.ws.rs.Produces; 
+import javax.ws.rs.core.MediaType; 
+@Path("/Hello") 
+
+
 public class ConcptionPaths {
 
+	
+	
+	
+	
 }
