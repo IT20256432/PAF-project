d52470bb07ec0e54e763830dad7a0297fba3c610
USER <USER@DESKTOP-290I1GM>
2022-04-25 23:49:30 +0530

    completed backend

--- ElectricityManagmentSystem/src/com/ConcptionPaths.java
+++ ElectricityManagmentSystem/src/com/ConcptionPaths.java
@@ -1,6 +1,9 @@
 package com;
 
-import model.Item;
+import model.ConceptionOpareters;
+
+//start
+//import javax.websocket.server.PathParam;
 //For REST Service
 import javax.ws.rs.*; 
 import javax.ws.rs.core.MediaType; 
@@ -10,11 +13,7 @@ import com.google.gson.*;
 import org.jsoup.*; 
 import org.jsoup.parser.*; 
 import org.jsoup.nodes.Document;
-//start
-//import javax.websocket.server.PathParam;
-import javax.ws.rs.GET; 
-import javax.ws.rs.Path; 
-import javax.ws.rs.Produces; 
+
 import javax.ws.rs.core.MediaType; 
 @Path("/Hello") 
 
@@ -22,7 +21,64 @@ import javax.ws.rs.core.MediaType;
 public class ConcptionPaths {
 
 	
+	ConceptionOpareters itemObj = new ConceptionOpareters();
+	@GET
+	@Path("/") 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String hello() 
+	 { 
+		
+		return itemObj.readItems();
+	 //return "Hello world chathu sa"; 
+	 } 
 	
+	
+	
+	@POST
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertItem(@FormParam("unit") String unit, 
+	 @FormParam("unifee") String unifee)
+	
+	  
+	{ 
+	 String output = itemObj.insertItem(unit, unifee); 
+	return output; 
+	}
+
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateItem(String itemData)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
+	//Read the values from the JSON object
+	 String id = itemObject.get("id").getAsString();
+	 String unit = itemObject.get("unit").getAsString();
+	 String unifee = itemObject.get("unifee").getAsString();
+	
+	 
+	 String output = itemObj.updateItem(id, unit, unifee);
+	return output;
+	}
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteItem(String itemData)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(itemData, "", Parser.xmlParser());
+
+	//Read the value from the element <itemID>
+	 String id = doc.select("id").text();
+	 String output = itemObj.deleteItem(id);
+	return output;
+	}
 	
 	
 }
